926f1ed0e0fc13eab00e8d6baecf56b27f661899
Benjamin DalFavero <bdalfavero@gmail.com>
2025-11-12 19:24:06 -0500

    Tried uniform sampling with the statevector instead of an MPS.

--- tensor_network_common.py
+++ tensor_network_common.py
@@ -76,3 +76,22 @@ def total_number_qubit_operator(n_orbitals: int, use_jw=True) -> of.QubitOperato
     else:
         total_number_qubit = of.transforms.bravyi_kitaev(total_number)
     return total_number_qubit
+
+
+def mps_to_vector(mps: MatrixProductState) -> np.ndarray:
+    """Convert an MPS into a normal vector. This assumes each index is a string
+    followed by a number, e.g. three indices 'k0, k1, k2'."""
+
+    def _idx_to_int(idx: str) -> int:
+        digits = [c for c in idx if c in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']]
+        if len(digits) == 0:
+            raise ValueError(f"Index {str} has no digits in it.")
+        return int(''.join(digits))
+    
+    # Contract the MPS into a tensor, then sort the indices. Convert that to a vector.
+    contracted_tensor = mps.contract()
+    sorted_inds = sorted(contracted_tensor.inds, key=_idx_to_int)
+    print(f"sorted_inds = {sorted_inds}")
+    contracted_tensor.transpose(*sorted_inds, inplace=True)
+    tensor_data = contracted_tensor.data
+    return tensor_data.reshape((tensor_data.size,))
